8183e2d2290f072ef131ee1f5ecaa958fe66378c
ngoluong1108 <103229403+luongngo1108@users.noreply.github.com>
2024-03-31 00:28:07 +0700

    finish function calculation number of hour for leave request

--- BackEnd/src/controllers/leaveRequestController.js
+++ BackEnd/src/controllers/leaveRequestController.js
@@ -38,7 +38,9 @@ class LeaveRequestController {
         var result = new ReturnResult();
         try {
             const model = req.body;
-            model.numberOfHour = calculateNumberOfHours(model.leaveDateFrom, model.leaveDateTo);
+            var leaveDateFrom = new Date(model?.leaveDateFrom);
+            var leaveDateTo = new Date(model?.leaveDateTo);
+            model.numberOfHour = calculateWorkingHour(leaveDateFrom, leaveDateTo);
             if (model.leaveRequestId === null || model.leaveRequestId === 0) { // Add new
                 model.status = leaveRequestStatus.WAITING;
                 const saveLeaveRequest = await dbContext.LeaveRequest.create({
@@ -151,32 +153,78 @@ async function handleLeaveEntitlementForLeaveRequest(userId, leaveEntitlementId)
     }
 }
 
-function calculateNumberOfHours(start, end) {
-    const startMoment = moment(start).add(7, 'hours');
-    const endMoment = moment(end).add(7, 'hours');
-  
-    const startRange1 = moment(startMoment).set({ hour: 8, minute: 30 });
-    const endRange1 = moment(startMoment).set({ hour: 12, minute: 0 });
-    const startRange2 = moment(startMoment).set({ hour: 13, minute: 0 });
-    const endRange2 = moment(startMoment).set({ hour: 17, minute: 30 });
-  
-    let workingHours = 0;
+function calculateWorkingHour(startDate, endDate, timeZone = 7) {
+    let workingHour = 0;
+    try {
+        if (startDate && endDate) {
+            startDate = new Date(startDate);
+            endDate = new Date(endDate);
+            startDate.setHours(startDate.getHours() + timeZone);
+            endDate.setHours(endDate.getHours() + timeZone);
+            while (startDate.getTime() <= endDate.getTime()) {
+                // Check is not Saturday or Sunday
+                if (startDate.getDay() === 6 || startDate.getDay() === 0) {
+                    startDate.setDate(startDate.getDate() + 1);
+                    startDate.setHours(8);
+                    startDate.setMinutes(30);
+                    continue;
+                }
 
-    if (endMoment.isAfter(startRange1) && startMoment.isBefore(endRange1)) {
-      const rangeStart = startMoment.isBefore(startRange1) ? startRange1 : startMoment;
-      const rangeEnd = endMoment.isAfter(endRange1) ? endRange1 : endMoment;
-      const duration = moment.duration(rangeEnd.diff(rangeStart));
-      workingHours += duration.asHours();
+                let estEndDate = new Date(startDate);
+                estEndDate.setDate(estEndDate.getDate() + 1);
+                estEndDate.setHours(0);
+                estEndDate.setMinutes(30);
+                
+                if (endDate.getTime() <= estEndDate.getTime()) estEndDate = endDate;
+                workingHour += calculateWorkingHourInDay(startDate, estEndDate);
+                startDate.setDate(startDate.getDate() + 1);
+                startDate.setHours(8);
+                startDate.setMinutes(30);
+            }
+        }
+    } catch (ex) {
+        console.error(ex);
     }
+    return Math.round(workingHour);
+}
 
-    if (endMoment.isAfter(startRange2) && startMoment.isBefore(endRange2)) {
-      const rangeStart = startMoment.isBefore(startRange2) ? startRange2 : startMoment;
-      const rangeEnd = endMoment.isAfter(endRange2) ? endRange2 : endMoment;
-      const duration = moment.duration(rangeEnd.diff(rangeStart));
-      workingHours += duration.asHours();
+function calculateWorkingHourInDay(startDate, endDate, timeZoneSetting = 7) {
+    let workingHour = 0;
+    try {
+        if (startDate.getUTCDate() === endDate.getUTCDate()) {
+            var morningStartTime = new Date(startDate);
+            morningStartTime.setHours(8 + timeZoneSetting);
+            morningStartTime.setMinutes(30);
+            var morningEndTime = new Date(startDate);
+            morningEndTime.setHours(12 + timeZoneSetting);
+            const morningWorkingHours = getOverlapDuration(morningStartTime, morningEndTime, startDate, endDate);
+            var afternoonStartTime = new Date(startDate);
+            afternoonStartTime.setHours(13 + timeZoneSetting);
+            var afternoonEndTime = new Date(startDate);
+            afternoonEndTime.setDate(afternoonEndTime.getDate() + 1);
+            afternoonEndTime.setHours(0);
+            afternoonEndTime.setMinutes(30);
+            const afternoonWorkingHours = getOverlapDuration(afternoonStartTime, afternoonEndTime, startDate, endDate);
+            const totalWorkingHours = morningWorkingHours + afternoonWorkingHours;
+            workingHour = totalWorkingHours;
+        }
+    } catch (ex) {
+        console.error(ex);
     }
-  
-    return workingHours;
+    return workingHour;
+}
+
+function getOverlapDuration(defaultStartDate, defaultEndDate, inputStartDate, inputEndDate) {
+    if (!overlaps(defaultStartDate, defaultEndDate, inputStartDate, inputEndDate))
+        return 0;
+    let overlapStart = inputStartDate.getTime() > defaultStartDate.getTime() ? inputStartDate : defaultStartDate;
+    let overlapEnd = inputEndDate.getTime() < defaultEndDate.getTime() ? inputEndDate : defaultEndDate;
+
+    return (overlapEnd.getTime() - overlapStart.getTime()) / (1000 * 60 * 60);
+}
+
+function overlaps(defaultStartDate, defaultEndDate, inputStartDate, inputEndDate) {
+    return inputStartDate.getTime() < defaultEndDate.getTime() && defaultStartDate.getTime() < inputEndDate.getTime();
 }
 
 export default new LeaveRequestController;
