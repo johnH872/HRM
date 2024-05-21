64b3ef8ca05d305a277f2a89d58565ec78a1c7e9
Hoang Ta <87011461+johnH872@users.noreply.github.com>
2024-05-21 23:41:02 +0700

    auto punch out job

---
+++ BackEnd/src/cronJobs/autoPunchOutJobs.js
@@ -0,0 +1,32 @@
+import { CronJob } from 'cron';
+import db from '../models/index.js';
+const dbContext = await db;
+
+const autoPunchOutJob = CronJob.from({
+    cronTime: '0 0 23 * * *',
+    onTick: async function () {
+        // Auto create work calendar in the first day of the month '0 0 1 * * *'
+        await handleAutoPunchOutJob();
+    },
+    start: false,
+    timeZone: 'Asia/Ho_Chi_Minh'
+});
+
+async function handleAutoPunchOutJob() {
+    try {
+        const [results, metadata] = await sequelize.query(`
+        UPDATE attendances 
+        SET punchoutDate = ADDTIME(STR_TO_DATE(CONCAT(DATE(punchinDate), '17:00:00'), '%Y-%m-%d %T'), '20:00:00.000000'),
+            punchoutTime = UNIX_TIMESTAMP(ADDTIME(STR_TO_DATE(CONCAT(DATE(punchinDate), ' 17:00:00'), '%Y-%m-%d %T'), '27:00:00.000000'))*1000,
+            punchoutOffset = -420,
+            punchoutNote = 'Auto punch out from the system'
+        WHERE punchoutDate IS NULL`);
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export {
+    autoPunchOutJob
+}
