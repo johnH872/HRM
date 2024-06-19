205cdc84f86893e8791e694695319e11490266d6
Hoang Ta <87011461+johnH872@users.noreply.github.com>
2024-06-19 22:26:28 +0700

    fix: allow FE build file for deployement

--- BackEnd/src/utils/redis.js
+++ BackEnd/src/utils/redis.js
@@ -1,4 +1,5 @@
 import Redis from "ioredis";
-const localRedis = new Redis("redis://:123@127.0.0.1:6379/1");
+// const localRedis = new Redis("redis://:123@127.0.0.1:6379/1");
+const localRedis = new Redis("redis://redis:6379/1");
 
 export default localRedis;
