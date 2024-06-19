abb606e4a7b26244315295d6da9971abb1113f99
Hoang Ta <87011461+johnH872@users.noreply.github.com>
2024-06-19 22:28:15 +0700

    Revert "fix: allow FE build file for deployement"
    This reverts commit 205cdc84f86893e8791e694695319e11490266d6.

--- BackEnd/src/utils/redis.js
+++ BackEnd/src/utils/redis.js
@@ -1,5 +1,4 @@
 import Redis from "ioredis";
-// const localRedis = new Redis("redis://:123@127.0.0.1:6379/1");
-const localRedis = new Redis("redis://redis:6379/1");
+const localRedis = new Redis("redis://:123@127.0.0.1:6379/1");
 
 export default localRedis;
